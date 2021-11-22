ee00d097e15a90fb7034f7bfcf9cca0f2c325311
Yan Linn Thor <2441118-yanlinnthor@users.noreply.gitlab.com>
2021-11-22 23:56:03 +0630

    Add ReactQueryDevtool

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { About } from "modules/About";
 import { Home } from "modules/Home";
 import React from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 import { Route, Routes } from "react-router-dom";
 
 const queryClient = new QueryClient();
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
       </Routes>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
--- src/modules/Users/hooks.ts
+++ src/modules/Users/hooks.ts
@@ -1,13 +1,15 @@
 import { useQuery } from "react-query";
 import { Users } from "./api";
 
+const config = { refetchOnWindowFocus: false };
+
 const getUsers = async () => {
   const { data } = await Users.index();
   return data;
 };
 
 export function useUsers() {
-  return useQuery("users.index", getUsers);
+  return useQuery("users.index", getUsers, config);
 }
 
 const getSingle = async () => {
